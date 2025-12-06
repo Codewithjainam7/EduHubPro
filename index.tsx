5bb26ca2e831c0e0ed6c9985c718202317a534cc
Jainam Jain <jainjainam412@gmail.com>
2025-12-06 17:44:58 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -7,9 +9,15 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
